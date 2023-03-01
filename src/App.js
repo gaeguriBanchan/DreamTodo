4683b37cffc66be4ca24d82031325e1a8a38121d
mochapot <rlawowls93@gamil.com>
2023-03-01 15:39:19 +0900

    필터 기능 완성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 import TodoList from './components/TodoList/TodoList';
 
 function App() {
+  const filters = ['all', 'active', 'completed'];
+  const [filter, setFilter] = useState(filters[0]);
   return (
     <div className="App">
-      <TodoList/>
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </div>
   );
 }
